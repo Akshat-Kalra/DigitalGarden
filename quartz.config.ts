02b9115cf00507607436a8664f940da061ab3722
Akshat-Kalra <akshatkalra2005@gmail.com>
2026-03-23 01:08:05 -0700

    Quartz sync: Mar 23, 2026, 1:08 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "akshat-kalra.github.io/DigitalGarden",
+    baseUrl: "akshat-kalra.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
